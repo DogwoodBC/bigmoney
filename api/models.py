e3735cbbbbd4403ad87ddceb87548b684d2eb8f6
hughstimson <hugh@hughstimson.org>
2016-08-05 16:22:53 -0700

    Sketches API models.

---
+++ api/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+
+class Donation(models.Model):
+    pass
+
+
+class Filer(models.Model):
+    pass
+
+
+class Affiliation(models.Model):
+    pass
+
+
+class FilerType(models.Model):
+    pass
+
+
+class ElectoralDistrict(models.Model):
+    pass
+
+
+class BoundarySet(models.Model):
+    pass
+
+
+class Contributor(models.Model):
+    pass
+
+
+class ContributorClass(models.Model):
+    pass
+
+
+class ContributorIndividual(models.Model):
+    pass
+
+
+class ContributorOrganization(models.Model):
+    pass
+
+
+class UniqueIndividuals(models.Model):
+    pass
+
+
+class UniqueOrganizations(models.Model):
+    pass

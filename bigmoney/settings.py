34e43d0ea989aaa187ae49f495cd6ff0252fbaaa
hughstimson <hugh@geocology.ca>
2016-08-25 16:27:32 -0700

    Adds a pointer to settings_staging for use on AWS.

--- bigmoney/settings.py
+++ bigmoney/settings.py
@@ -6,6 +6,8 @@ server = node()
 
 if 'badger' in server:
     from bigmoney.settings_badger import *
+elif 'ip-172-31-15-13' in server:
+    from bigmoney.settings_staging import *
 elif 'ip-172-31-21-189' in server:
     from bigmoney.settings_endpoint import *
 else:

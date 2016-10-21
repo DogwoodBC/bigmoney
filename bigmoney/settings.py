dcfa2af93fc39292a404c2beeda79c2b937b171b
hughstimson <hugh@geocology.ca>
2016-10-20 18:15:19 -0700

    Updates settings to (hopefully) get static file collection working.

--- bigmoney/settings.py
+++ bigmoney/settings.py
@@ -1,14 +1,19 @@
 from platform import node
-
-from bigmoney.settings_base import *
-
 try:
     server = node()
 
-    if 'badger' in server:
-        from bigmoney.settings_badger import *
-    elif 'ip-172-31-21-189' in server:
-        from bigmoney.settings_endpoint import *
+    # Normally EB should skip bigmoney.settings and load bigmoney.settings_eb_staging directly,
+    # but in case bigmoney.settings is getting loaded by manage.py on the EB server:
+    if 'ip-172-31-30-24' in server:
+        from bigmoney.settings_eb_staging import *
+
+    else:
+        from bigmoney.settings_base import *
+
+        if 'badger' in server:
+            from bigmoney.settings_badger import *
+        elif 'ip-172-31-21-189' in server:
+            from bigmoney.settings_endpoint import *
 
 except:
     raise Exception("Problem in settings.py.")

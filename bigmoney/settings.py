86458e615dd506fbd5dee7d77b9c0b4fa131bfd6
hughstimson <hugh@geocology.ca>
2016-10-20 17:28:33 -0700

    Splits Elastic Beanstalk settings off into a standalone file.
    Further effort to try and get settings loaded on EB.

--- bigmoney/settings.py
+++ bigmoney/settings.py
@@ -2,8 +2,6 @@ from platform import node
 
 from bigmoney.settings_base import *
 
-print('Loading settings.')
-
 try:
     server = node()
 
@@ -11,9 +9,6 @@ try:
         from bigmoney.settings_badger import *
     elif 'ip-172-31-21-189' in server:
         from bigmoney.settings_endpoint import *
-    # Otherwise assume (for now) that this is the staging Elastic Beanstalk environment.
-    else:
-        from bigmoney.settings_template import *
 
 except:
     raise Exception("Problem in settings.py.")

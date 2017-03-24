d9c8554f8a6dadf4b44f2d8a5c9c581794b8b743
Ryder Bergerud <ryder.bergerud@gmail.com>
2017-03-23 20:39:28 -0700

    Adds cors so can work frontend locally
    Added cors support for localhost so server can run locally and respond to XHR requests from localhosts as we work on frontend

--- bigmoney/settings.py
+++ bigmoney/settings.py
@@ -31,17 +31,25 @@ INSTALLED_APPS = [
     'rest_framework_docs',
     'bigmoney',
     'data',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+    '127.0.0.1:8080'
+)
+
 
 ROOT_URLCONF = 'bigmoney.urls'
 
